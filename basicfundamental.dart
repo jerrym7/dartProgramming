fa8f8858d95f88f36dd220a80801622fe86f9586
jerrym7 <diino4l@gmail.com>
2019-03-26 15:47:46 -0700

    basic fundamentals of dart programming

--- basicfundamental.dart
+++ basicfundamental.dart
@@ -21,7 +21,7 @@ class Person{
     this._heightcm = heightcm;
     this._ssn = validateSSN(ssn);
   }
-  //
+  //validate ssn with '-'
 String validateSSN(var ssn){
   if(ssn[3] == "-" && this._ssn.length == 11 && ssn[6] == "-"){
     return ssn;
@@ -32,39 +32,39 @@ String validateSSN(var ssn){
     return validateSSN(ssn);
   }
 }
-//
+//set name
 setName(var name){
   this.name = name;
 }
-//
+//get name, returns string
 getName(){
   return this.name;
 }
-//
+//set age
 setAge(var age){
   this._age = age;
 }
-//
+//get age for the person
 getAge(){
   return this._age;
 }
-//
+//set height in cm
 setHeight(var cm){
   this._heightcm = cm;
 }
-//
+//get the height in cm
 getHeight(){
   return this._heightcm;
 }
-//
+//set social security number
 setSSN(var ssn){
   this._ssn = ssn;
 }
-//
+//get social security number
 getSSN(){
   return this._ssn;
 }
-//
+//overide toString() method.
 toString(){
   return "Name: " + this.name + "\n" +"Age: " + this._age.toString() + "\n" + "Height: " + this._heightcm.toString() +" cm\n" + "SSN: " + this._ssn;
 }
